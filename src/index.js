efbe53e8a5275855e90a05e4acee545b993c74c3
danilopolani <danilo.polani@gmail.com>
2021-07-07 23:49:57 +0200

    fix injection

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import bootstrap from './bootstrap';
-import config, { update } from './config';
+import { update } from './config';
 import lib from './lib';
 
 export default function install (Vue, options = {}) {
   update({ ...options, $vue: Vue });
 
   // Create a deafult empty function to prevent errors
-  config.$vue.prototype.$nop = config.$vue.$nop = lib;
+  Vue.prototype.$nop = Vue.$nop = lib
 
   bootstrap();
 }
